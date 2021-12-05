ed00a98ccf1ff425c41e3e1e7af692386321ffa9
AlexZig <62720497+AlexZig@users.noreply.github.com>
2021-12-06 00:12:45 +0300

    Update sctipt.js

--- js/sctipt.js
+++ js/sctipt.js
@@ -1,6 +1,23 @@
 let clock = document.querySelector('.clock');
 let startTime = new Date();
 let startSecond = startTime.getSeconds().toString();
+let settings = document.querySelector('#settings');
+let alarm = document.querySelector('#alarm');
+let reminder = document.querySelector('#reminder');
+let formAlarm = document.querySelector('.form__alarm');
+let formReminder = document.querySelector('.form__reminder');
+let formAlarmClouse = document.querySelector('#formAlarmClouse');
+let formReminderClouse = document.querySelector('#formReminderClouse');
+let alarmHourInput = document.querySelector('#alarmHourInput');
+let alarmMinuteInput = document.querySelector('#alarmMinuteInput');
+let alarmInstall = document.querySelector('#alarmInstall');
+let alarmHour = "";
+let alarmMinute = "";
+let alarmArrow = document.querySelector('.alarm-arrow');
+let alarmArrowPosition;
+let reminderMinuteInput = document.querySelector('#reminderMinuteInput');
+let reminderInstall = document.querySelector('#reminderInstall');
+let reminderMinute = "";
 let digitalClock = document.querySelector('#time');
 let audio = document.createElement('audio');
 audio.setAttribute('src' , './audio/Увидомление.mp3');
@@ -9,7 +26,10 @@ function digitalClockNow() {
   let h = time.getHours().toString();
   let m = time.getMinutes().toString();
   let s = time.getSeconds().toString();
-  if (m == 50 && s == 0) {
+  if (h == alarmHour && m == alarmMinute && s == 0) {
+    audio.play();
+  }
+  if ( m == reminderMinute && s == 0) {
     audio.play();
   }
   if (h.length < 2) {
@@ -47,9 +67,50 @@ function secondControl() {
     clock.classList.add('clock_active');
     digitalClock.innerHTML = h + ':' + m + ':' + s;
     setInterval(digitalClockNow, 1);
-  }
-}
+  };
+};
 function start() {
   startInterval = setInterval(secondControl, 1);
-}
-setTimeout(start, 1000)
+};
+setTimeout(start, 1000);
+settings.onclick = function() {
+  settings.classList.toggle('settings_active');
+};
+alarm.onclick = function() {
+  formAlarm.classList.add('form_active');
+  settings.classList.add('inactive');
+};
+reminder.onclick = function() {
+  formReminder.classList.add('form_active');
+  settings.classList.add('inactive');
+};
+formAlarmClouse.onclick = function() {
+  formAlarm.classList.remove('form_active');
+  settings.classList.remove('inactive');
+};
+formReminderClouse.onclick = function() {
+  formReminder.classList.remove('form_active');
+  settings.classList.remove('inactive');
+};
+alarmInstall.onclick = function() {
+  if (parseInt(alarmHourInput.value) > 23 || parseInt(alarmHourInput.value) < 0 || parseInt(alarmMinuteInput.value) > 59 || parseInt(alarmMinuteInput.value) < 0) {
+    alert('Вы ввели некорректные значения');
+  }else{
+    formAlarm.classList.remove('form_active');
+    settings.classList.remove('inactive');
+    alarmHour = alarmHourInput.value;
+    alarmMinute = alarmMinuteInput.value;
+    alarmArrowPosition = alarmHour * 30 + (alarmMinute * 0.5);
+    clock.style.setProperty('--alarmArrowPosition', alarmArrowPosition + 'deg');
+    alarmArrow.classList.add('alarm-arrow_active');
+  };
+};
+reminderInstall.onclick = function() {
+  if (parseInt(reminderMinuteInput.value) > 59 || (parseInt(reminderMinuteInput.value)) < 0) {
+    alert('Вы ввели некорректные значения');
+  }else{
+    formReminder.classList.remove('form_active');
+    settings.classList.remove('inactive');
+    reminderMinute = reminderMinuteInput.value;
+  };
+};
